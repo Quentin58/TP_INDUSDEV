d5765608e4ec25ebc250c3eb74bbc0d21612b5a6
joris pointe <joris.pointe@outlook.fr>
2020-07-07 18:24:53 +0200

    Joris 07/07/2020 18:24 Début de la page de connexion

---
+++ frmConnection.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TP_INDUSDEV
+{
+    partial class frmConnection
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubTitle = new System.Windows.Forms.Label();
+            this.tbxLogin = new System.Windows.Forms.TextBox();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(166, 63);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(449, 55);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Ticket Manager";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSubTitle
+            // 
+            this.lblSubTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubTitle.Location = new System.Drawing.Point(312, 118);
+            this.lblSubTitle.Name = "lblSubTitle";
+            this.lblSubTitle.Size = new System.Drawing.Size(160, 31);
+            this.lblSubTitle.TabIndex = 1;
+            this.lblSubTitle.Text = "Connexion";
+            this.lblSubTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbxLogin
+            // 
+            this.tbxLogin.Location = new System.Drawing.Point(318, 212);
+            this.tbxLogin.Name = "tbxLogin";
+            this.tbxLogin.Size = new System.Drawing.Size(139, 20);
+            this.tbxLogin.TabIndex = 2;
+            this.tbxLogin.Text = "Identifiant";
+            this.tbxLogin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(318, 247);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(139, 20);
+            this.tbxPassword.TabIndex = 3;
+            this.tbxPassword.Text = "Mot de passe";
+            this.tbxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Se connecter";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmConnection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.tbxLogin);
+            this.Controls.Add(this.lblSubTitle);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "frmConnection";
+            this.Text = "Page de connexion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubTitle;
+        private System.Windows.Forms.TextBox tbxLogin;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
--- frmConnection.cs
+++ frmConnection.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TP_INDUSDEV
 {
-    public partial class Form1 : Form
+    public partial class frmConnection : Form
     {
-        public Form1()
+        public frmConnection()
         {
             InitializeComponent();
         }
