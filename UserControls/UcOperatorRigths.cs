a5735b7893a1b269e92dfa4707811fc740efc527
Quentin Chaumet <quentinchaumet@yahoo.fr>
2020-07-08 14:55:58 +0200

    Quentin Chaumet 08/07/2020 Modification UcOperatorRights Modification UcManageRigths

---
+++ UserControls/UcOperatorRigths.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TP_INDUSDEV.Data;
+
+namespace TP_INDUSDEV.UserControls
+{
+    public partial class UcOperatorRigths : UserControl
+    {
+
+        // Variables
+        #region Variables
+
+        T_OPERATOR_TYPE OperatorType;
+        List<T_RIGHT> liRight;
+        List<TJ_RIGHT_OPERATOR_TYPE> liRightOperatorType;
+
+        #endregion
+
+        // Constructeur
+        public UcOperatorRigths()
+        {
+            InitializeComponent();
+        }
+        public UcOperatorRigths(T_OPERATOR_TYPE operatorType, List<T_RIGHT> liRight, List<TJ_RIGHT_OPERATOR_TYPE> liRightOperatorType)
+        {
+            InitializeComponent();
+
+            this.OperatorType = operatorType;
+            this.liRight = liRight;
+            this.liRightOperatorType = liRightOperatorType;
+        }
+
+
+        // Méthodes
+        #region Méthodes
+        #endregion
+
+        // Evenements
+        #region Evenements
+
+        #endregion
+    }
+}
